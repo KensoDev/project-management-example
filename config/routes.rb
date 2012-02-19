13a8ac1010661a4f0220a7a42d926ae59789d2cc
Avi Tzurel <avi@kensodev.com>
2012-02-19 23:32:56 +0200

    Twitter assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ProjectManagementExample::Application.routes.draw do
-  get "home/index"
+  root :to => 'home#index'  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
