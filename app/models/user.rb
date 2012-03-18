930b39134ffcdec9e30f68e9102786533fc9aa5d
Avi Tzurel <avi@kensodev.com>
2012-03-18 23:47:57 +0200

    Finalize Chapter 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :subdomain, :first_name, :last_name
+
+
+  validates_uniqueness_of :subdomain
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validations" do
+    it "should validate the uniqueness of subdomain" do
+      @user = User.new(first_name: Faker::Name.first_name, 
+        last_name: Faker::Name.last_name,
+        password: "foobar",
+        password_confirmation: "foobar",
+        email: Faker::Internet.email,
+        subdomain: "kensodev")
+      @user.save.should be_true
+
+      @other_user = User.new(first_name: Faker::Name.first_name, 
+        last_name: Faker::Name.last_name,
+        password: "foobar",
+        password_confirmation: "foobar",
+        email: Faker::Internet.email,
+        subdomain: "kensodev")
+
+      @other_user.save.should be_false
+    end
+
+    
+  end
 end
