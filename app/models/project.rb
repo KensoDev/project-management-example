bd6c385f2e65b4f783cdadb69968910b39d0b8c3
Avi Tzurel <avi@kensodev.com>
2012-04-02 00:16:53 +0300

    Project name, status, specs

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,13 @@
 class Project < ActiveRecord::Base
+  validates_presence_of :name, :status
+
+  STATUS = { inactive: 0, active: 1, pending: 2, archived: 3 }
   
+  def status
+    STATUS.key(read_attribute(:status))
+  end
+
+  def status=(s)
+    write_attribute(:status, STATUS[s])
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,13 +1,43 @@
 require 'spec_helper'
 
 describe Project do
-  it "should have a name" do
-    project = Project.new
-    project.should respond_to(:name)
+  describe "Params" do
+    it "should have a name" do
+      project = Project.new
+      project.should respond_to(:name)
+    end
+
+    it "should have a status" do
+      project = Project.new
+      project.should respond_to(:status)
+    end
   end
 
-  it "should have a status" do
-    project = Project.new
-    project.should respond_to(:status)
+  describe "Validations" do
+    before(:each) do
+      @valid_params = { name: "Project 01", status: :active }
+    end
+
+    it "should not save project without a name" do
+      project = Project.new(@valid_params.merge({name: ''})) 
+      project.save.should be_false
+    end
+
+    it "should not save project without a status" do
+      project = Project.new(@valid_params.merge({status: ''})) 
+      project.save.should be_false
+    end
+  end
+
+  describe "Status" do
+    before(:each) do
+      @valid_params = { name: "Project 01", status: :active }
+    end
+
+    it "should save the status correctly to the database" do
+      project = Project.new(@valid_params)
+      project.save
+      project.status.should eq(:active)
+    end
   end
 end
