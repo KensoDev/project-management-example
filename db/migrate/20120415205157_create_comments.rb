b6e3941583a0fc79a9b5f5bd1837467fc6115cab
Avi Tzurel <avi@kensodev.com>
2012-04-15 23:53:07 +0300

    Comment model added to the project

---
+++ db/migrate/20120415205157_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.text :body
+      t.integer :user_id
+      t.integer :item_id
+      t.string :item_type
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120415204118) do
+ActiveRecord::Schema.define(:version => 20120415205157) do
+
+  create_table "comments", :force => true do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.integer  "item_id"
+    t.string   "item_type"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "projects", :force => true do |t|
     t.string   "name"
