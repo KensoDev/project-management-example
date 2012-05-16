c05b3ccd3b0d8a4d9af56840ce55c102fd969689
Avi Tzurel <avi@kensodev.com>
2012-05-16 14:06:54 +0300

    User is being redirected to the account root after sign_in or sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def after_sign_in_path_for(resource)
+    if resource.is_a? User
+      return root_url(subdomain: resource.subdomain)
+    else
+      super
+    end
+  end
+
+  
 end
